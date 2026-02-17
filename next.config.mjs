9f944c86c24360b2b87acc7e93043e0fd980a83a
Prady Neog <pradyneo@users.noreply.github.com>
2026-02-16 20:48:05 -0800

    Fix next js config for github pages build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '',
+  trailingSlash: true,
+};
 
 export default nextConfig;
