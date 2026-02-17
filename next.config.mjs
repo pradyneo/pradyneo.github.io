2355e777f26029c6cb3eb20afd08a6895031ec94
Prady Neog <pradyneo@users.noreply.github.com>
2026-02-16 21:37:34 -0800

    Add .nojekyll file; update base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '',
   trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
